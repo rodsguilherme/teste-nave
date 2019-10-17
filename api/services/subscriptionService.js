ff30d3becf2bd109dd8ede3eef0cfd0a59d86559
guilherme <rodsguilherme@gmail.com>
2019-10-17 15:08:25 -0300

    Adiciona inscricao do candidato na vaga

--- api/services/subscriptionService.js
+++ api/services/subscriptionService.js
@@ -6,7 +6,7 @@ import database from '../database/connectDB';
 
 const createSubscription = async (dataSubscription) => {
 
-    const { nameVacancy, nameCandidate, cpfCandidate } = dataSubscription;
+    const { nameVacancy, cpfCandidate } = dataSubscription;
 
     if (!nameVacancy) {
         throw ("Verifique os campos!");
@@ -14,25 +14,23 @@ const createSubscription = async (dataSubscription) => {
     if (validate(cpfCandidate) === false) {
         throw ("Digite um CPF válido!");
     }
-    const selectVacancy = 'SELECT * FROM Vacancy WHERE name = ?'
-    const vacancysSelecteds = await database.get(selectVacancy, [nameVacancy]);
-    if (vacancysSelecteds == null) {
-        throw ("Nenhuma vaga com esse nome foi encontrada.");
-    }
-    const selectCandidate = 'SELECT * FROM Candidate WHERE name = ?';
-    const candidatesSelected = await database.get(selectCandidate, [nameCandidate]);
+    const selectCandidatebyCpf = 'SELECT idCandidate FROM Candidate WHERE cpf = ?';
+    const candidatesSelected = await database.get(selectCandidatebyCpf, [cpfCandidate]);
     if (candidatesSelected == null) {
-        throw ("Nenhum candidato com esse nome foi encontrado.");
+        throw ("Nenhum candidato com esse CPF foi encontrado.");
     }
     const match = verifyHash(cpfCandidate, candidatesSelected.cpf);
     if (match === false) {
         throw ("Candidato já cadastrado na vaga.");
     }
+
     const selectSubscription = 'SELECT * FROM Subscription WHERE idVacancy = ? and idCandidate = ?';
     const subscriptionSelected = await database.get(selectSubscription, [vacancysSelecteds.idVacancy, candidatesSelected.idCandidate]);
+
     if (subscriptionSelected != null) {
         throw ("Candidato já cadastrado na vaga.");
     }
+    
     const insert = 'INSERT INTO Subscription (idVacancy, idCandidate) VALUES (?, ?)';
     await database.run(insert, [vacancysSelecteds.idVacancy, candidatesSelected.idCandidate]);
 
