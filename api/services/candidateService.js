258c7d7ad7290ff938f1be9fa04531111314c553
GuilhermeRods <rodsguilherme@gmail.com>
2019-10-18 00:11:47 -0300

    Atualiza o cadastro de candidatos

--- api/services/candidateService.js
+++ api/services/candidateService.js
@@ -2,38 +2,57 @@ import database from '../database/connectDB';
 import { validate } from 'cpf-check';
 import { emailValidation, telephoneValidation } from '../services/validationService';
 
+
 const createCandidate = async dataCandidate => {
 
     const { name, email, telephone, cpf } = dataCandidate;
 
-    if(emailValidation(email) === false) {
-        throw ("Insira um email válido.");
+    const matched = await verifyCandidate(dataCandidate);
+
+
+    if (matched == false) {
+        throw ("Usuário já cadastrado");
     }
 
-    if(telephoneValidation(telephone) === false) {
-        throw ("Insira um telefone válido.");
-    }
-
-    if (validate(cpf) === false) {
-        throw ("Insira um CPF válido.");
-    }
-
-    const selectByEmail = 'SELECT idCandidate FROM Candidate WHERE email = ?';
-    const row = await database.get(selectByEmail, [email]);
-
-    if (row != null) {
-        throw ("Esse email já foi cadastrado, tente novamente com outro.");
-    }
-    const selectByCpf = 'SELECT idCandidate FROM Candidate WHERE cpf = ?';
-    const candidateSelected = await database.get(selectByCpf, [cpf]);
-
-    if (candidateSelected !== null) {
-        throw ("Usuário já cadastrado.");
-    }
     const insert = 'INSERT INTO Candidate (name, email, telephone, cpf) VALUES (?, ?, ?, ?)';
     await database.run(insert, [name, email, telephone, cpf]);
 
 };
 
+const verifyCandidate = async candidateChecked => {
+    const { name, email, telephone, cpf } = candidateChecked;
+
+    if (!name) {
+        throw ("Insira o nome.");
+    }
+    if (validate(cpf) == false) {
+        throw ("Insira um CPF valido");
+    }
+
+    const emailChecked = emailValidation(email);
+    if (emailChecked == false) {
+        throw ("Insira um email valido");
+    }
+
+    const telephoneChecked = telephoneValidation(telephone);
+    if (telephoneChecked == false) {
+        throw ("Insira um telefone valido.");
+    }
+
+    const searchCandidateByCpf = 'SELECT idCandidate FROM Candidate WHERE cpf = ?';
+    const candidateMatchedByCpf = await database.get(searchCandidateByCpf, [cpf]);
+
+    if (candidateMatchedByCpf !== undefined) {
+        throw ("CPF já cadastrado");
+    }
+
+    const searchCandidateByEmail = 'SELECT idCandidate FROM Candidate WHERE email = ?';
+    const candidateMatchedByEmail = await database.get(searchCandidateByEmail, [email]);
+
+    if (candidateMatchedByEmail !== undefined) {
+        throw ("Email já cadastrado.")
+    }
+    return true;
+}
 
 module.exports = { createCandidate };
