6600dbcad65ef514459b230088095eddb310decf
GuilhermeRods <rodsguilherme@gmail.com>
2019-10-17 22:34:20 -0300

    Adiciona validacao de data

--- api/services/validationService.js
+++ api/services/validationService.js
@@ -7,8 +7,13 @@ const emailValidation = email => {
 };
 
 const telephoneValidation = telephone => {
-    const telephoneRegex = '^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$';
+    const telephoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
     return telephoneRegex.test(telephone).parseInt()
-       
+
 };
-module.exports =  { emailValidation, telephoneValidation };
+
+const dateValidaton = date => {
+    const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+    return dateValidaton.test(dateRegex);
+}
+module.exports = { emailValidation, telephoneValidation, dateValidaton };
