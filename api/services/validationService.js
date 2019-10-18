0a072d9678167c9116967abaa9da66b991ed63c2
GuilhermeRods <rodsguilherme@gmail.com>
2019-10-18 02:26:08 -0300

    Atualiza a validacao de email e telefone

--- api/services/validationService.js
+++ api/services/validationService.js
@@ -2,18 +2,15 @@
 
 const emailValidation = email => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(String(email).toLowerCase())
+   return emailRegex.test(String(email).toLowerCase())
 
 };
 
 const telephoneValidation = telephone => {
-    const telephoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
-    return telephoneRegex.test(telephone).parseInt()
+    const telephoneRegex = /\(\d{2,}\) \d{4,}\-\d{4}/;
+    return telephoneRegex.test(telephone);
 
 };
 
-const dateValidaton = date => {
-    const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
-    return dateValidaton.test(date);
-}
-module.exports = { emailValidation, telephoneValidation, dateValidaton };
+
+module.exports = { emailValidation, telephoneValidation};
