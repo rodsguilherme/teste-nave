a78a2b04426c016cb232c50769c67d8dd776788c
GuilhermeRods <rodsguilherme@gmail.com>
2019-10-19 11:49:15 -0300

    Retorna comentario pelo ID

--- api/controllers/commentaryController.js
+++ api/controllers/commentaryController.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 import { verifyJWT } from '../services/authService';
-import { createCommentary } from '../services/commentaryService';
+import { createCommentary, getCommentaryByIdSubs } from '../services/commentaryService';
 
 router.post('/', verifyJWT, async (request, response) => {
     const dataCommentary = {
@@ -16,6 +16,17 @@ router.post('/', verifyJWT, async (request, response) => {
     } catch (error) {
         response.status(400).send({ error: 'Não foi possivel criar o comentário' });
     }
+});
+
+router.get('/:id', verifyJWT, async (request, response) => {
+    const idSubs = request.params.id;
+
+    const commentarys = await getCommentaryByIdSubs(idSubs);
+
+    if (commentarys)
+        response.status(200).send(commentarys);
+    else
+        response.status(404).send({ error: 'Inscrição sem comentarios' });
 });
 
 module.exports = (api) => api.use('/api/admin/comentario', router)
--- api/services/commentaryService.js
+++ api/services/commentaryService.js
@@ -12,9 +12,35 @@ const createCommentary = async dataCommentary => {
 };
 
 const commentaryIsValid = commentary => {
-    if (!commentary || typeof(commentary) !== "string") {
+    if (!commentary || typeof (commentary) !== "string") {
         return false;
+    }
+    return true;
+};
+
+const commentaryExistsByIdSubs = async idSubs => {
+
+    if (!idSubs || idSubs <= 0) {
+        return false;
+    }
+    const searchById = 'SELECT idSubs FROM Commentary WHERE idSubs = ?';
+    const subs = await database.all(searchById, [idSubs]);
+
+    if (subs.length == 0) {
+        return false;
+    }
+    return true;
+};
+
+const getCommentaryByIdSubs = async idSubs => {
+    const commentaryChecked = await commentaryExistsByIdSubs(idSubs);
+
+    if (commentaryChecked) {
+        const searchByIdSubs = 'SELECT commentary FROM Commentary WHERE idSubs = ?';
+        const idSubsChecked = await database.all(searchByIdSubs, [idSubs]);
+
+        return idSubsChecked;
     }
 };
 
-module.exports = { createCommentary };
+module.exports = { createCommentary, getCommentaryByIdSubs };
